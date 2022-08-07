8d2e92d28ef750ee3b4863c4c2551be62289fdbb
Victor Bo <10667379@qq.com>
2022-08-07 22:09:22 +0800

    chore: add utils docs

---
+++ docs/.vitepress/sidebar.ts
@@ -0,0 +1,7 @@
+export default [
+  { text: 'Array', link: '/method/array' },
+  { text: 'Color', link: '/method/color' },
+  { text: 'Fn', link: '/method/fn' },
+  { text: 'Is', link: '/method/is' },
+  { text: 'String', link: '/method/string' },
+]
